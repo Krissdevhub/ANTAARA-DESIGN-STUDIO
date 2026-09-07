9610a3deb3940f2cd226da254b5f01f55d93c0db
krissdevhub <krissdevhub@gmail.com>
2026-09-07 23:59:45 +0530

    chore: clean standard next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  outputFileTracingExcludes: {
-    "*": [
-      "extracted_assets/**/*",
-      "node_modules/@swc/core-win32-x64-msvc/**/*",
-      "node_modules/@esbuild/win32-x64/**/*",
-    ],
-  },
 };
 
 export default nextConfig;
